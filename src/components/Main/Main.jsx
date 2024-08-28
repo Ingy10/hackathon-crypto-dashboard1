5e7f6d28d6f19cde9acacf9ab4dd0da1446ee192
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-08-28 16:43:25 +0000

    added axios

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,24 @@
 import "./Main.scss";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Main() {
   const [cryptos, setCryptos] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.coincap.io/v2/assets")
-      .then((response) => response.json())
-      .then((data) => setCryptos(data.data))
-      .catch((error) => console.error("Error fetching data:", error));
+    const getData = async () => {
+      try {
+        const response = await axios.get('https://api.coincap.io/v2/assets');
+        setCryptos(response.data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getData();
   }, []);
+
+  
   return (
     <>
       <main className="main">
@@ -19,6 +28,7 @@ function Main() {
             {cryptos.map((crypto) => (
               <li key={crypto.id}>
                 {crypto.name} - ${parseFloat(crypto.priceUsd).toFixed(2)}
+                {/* {crypto.symbol} - ${crypto.priceUsd} */}
               </li>
             ))}
           </ul>
